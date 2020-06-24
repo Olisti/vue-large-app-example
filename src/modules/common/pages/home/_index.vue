e8b9d5c2cbcadfa474a7ae35d23958347b6eabbb
Svetlana <gerbophilia@gmail.com>
2020-06-24 17:58:34 +0300

    home page

--- src/modules/common/pages/home/_index.vue
+++ src/modules/common/pages/home/_index.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>Home Page</div>
+    <div class="home">
+        <span class="home__title">vue-large-app-example</span>
+        <a
+            class="home__link ol-link"
+            href="https://github.com/Olisti/vue-large-app-example"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <img class="home__logo" src="@/assets/github-logo.png" alt="GitHub logo" />
+            <span class="home__text">to GitHub Repository</span>
+        </a>
+    </div>
 </template>
 
 <script lang="ts">
@@ -9,4 +20,27 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class Home extends Vue {}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '~@/styles/variables';
+.home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    &__title {
+        text-transform: uppercase;
+        font-weight: 600;
+        font-size: 24px;
+        color: $color-main-20;
+        margin-bottom: 20px;
+    }
+    &__link {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    &__logo {
+        height: 150px;
+        max-width: 150px;
+    }
+}
+</style>
