2ec8369765b55b82d8dbe523c7128d4376aef1fb
Svetlana <gerbophilia@gmail.com>
2020-07-27 20:42:39 +0300

    layout fixes

--- src/app/components/app-header.vue
+++ src/app/components/app-header.vue
@@ -25,6 +25,8 @@ export default class AppHeader extends Vue {
 header {
     position: sticky;
     top: 0;
+    left: 0;
+    right: 0;
     display: flex;
     align-items: center;
     min-height: 56px;
